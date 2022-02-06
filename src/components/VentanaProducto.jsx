0ccac53cc62b70cffae2eeaa139345bfdb4622aa
leonardog9823 <guanuco43@gmail.com>
2022-02-06 02:56:14 -0300

    animacion item oferta

--- src/components/VentanaProducto.jsx
+++ src/components/VentanaProducto.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import "../styles/Item.scss"
-import { faBolt } from '@fortawesome/free-solid-svg-icons';
+import { faBolt, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
-export const VentanaProducto = ({ img }) => {
+export const VentanaProducto = ({ img, titulo }) => {
     
     return (
-        <div className='item'>
-            <div>
-                <img src={ img } className="img-item"/>
-            </div>
-            <div className='item__info'>
-                <div className='info-numero'>
-                    <p className='info-precio'> $ 4.999 </p>
-                    <p className='info-descuento'>40% OFF</p>
+            <Link to="/" className='item'>
+                <div>
+                    <img src={ img } className="img-item"/>
                 </div>
-                <p className='item-cuotas'>6x $ 999.83 sin interés</p>
-                <div className='item-envio'>
-                    <p>Envío gratis </p>
-                    <FontAwesomeIcon className='icon-full' icon={ faBolt }/>
-                    <p className='envio-full'>FULL</p>
+                <div className='item__info'>
+                    <div className='info-numero'>
+                        <p className='info-precio'> $ 4.999 </p>
+                        <p className='info-descuento'>40% OFF</p>
+                    </div>
+                    <p className='item-cuotas'>6x $ 999.83 sin interés</p>
+                    <div className='item-envio'>
+                        <p>Envío gratis </p>
+                        <FontAwesomeIcon className='icon-full' icon={ faBolt }/>
+                        <p className='envio-full'>FULL</p>
+                    </div>
+                    <p className='titulo-hover'>{ titulo }</p>
                 </div>
-            </div>
-        </div>
+                <div className='icon-fav'>
+                    <FontAwesomeIcon className='' icon={ faHeart }/>
+                </div>
+            </Link>
     )
 };
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -6,11 +6,11 @@ import "../styles/flex-item.scss";
 import "../styles/home.scss";
 
 const ofertas = [
-    { id: 1, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, envio: { disponible: true, tipo: "basico"} },
-    { id: 2, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, envio: { disponible: true, tipo: "completo"} },
-    { id: 3, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, envio: { disponible: false, tipo: "null"} },
-    { id: 4, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, envio: { disponible: true, tipo: "basico"} },
-    { id: 5, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, envio: { disponible: true, tipo: "basico"} }
+    { id: 1, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, titulo: "Heladera w1321 asdas da 21321 2e", envio: { disponible: true, tipo: "basico"} },
+    { id: 2, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, titulo: "Heladera w1321 asdas da 21321 2e", envio: { disponible: true, tipo: "completo"} },
+    { id: 3, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, titulo: "Heladera w1321 asdas da 21321 2e", envio: { disponible: false, tipo: "null"} },
+    { id: 4, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, titulo: "Heladera w1321 asdas da 21321 2e", envio: { disponible: true, tipo: "basico"} },
+    { id: 5, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true}, titulo: "Heladera w1321 asdas da 21321 2e", envio: { disponible: true, tipo: "basico"} }
 ]
 
 export const Home = () => {
