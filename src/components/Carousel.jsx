de8cfe32689eb2b7bbffee084af1066c3e49b4fd
leonardog9823 <guanuco43@gmail.com>
2022-02-05 22:36:17 -0300

    carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import "../styles/Carousel.scss";
+
+export const Carousel = () => {
+    return (
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active btn--slide" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" className="btn--slide" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" className="btn--slide" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="../../assets/D_NQ_722060-MLA48959399715_012022-OO.webp" className="d-block w-100 img-slide" alt="slide-1"/>
+                </div>
+                <div className="carousel-item">
+                    <img src="../../assets/D_NQ_829565-MLA48927896565_012022-OO.webp" className="d-block w-100 img-slide" alt="slide-2"/>
+                </div>
+                <div className="carousel-item">
+                    <img src="../../assets/D_NQ_894113-MLA49020869660_022022-OO.webp" className="d-block w-100 img-slide" alt="slide-3"/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+};
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Carousel } from '../components/Carousel';
+
+const ofertas = [
+    { id: 1, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true} },
+    { id: 1, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true} },
+    { id: 1, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true} },
+    { id: 1, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true} },
+    { id: 1, img: "../../assets/productos/D_Q_NP_935558-MLA45017514805_022021-AB.webp", precio: "5499", oferta: { precioAnterior: "8000", disponible: true} }
+]
 
 export const Home = () => {
+
+    const [ofertasDisponibles, setOfertasDisponibles] = useState([]);
+    
+    const ofertasHome = () => {
+        const encontrarOferta = ofertas.filter( o => o.oferta.disponible === true )
+        setOfertasDisponibles(encontrarOferta);
+    }
+
+    useEffect( () => {
+        ofertasHome()
+    }, [])
+
     return (
-        <div>
-            home
-        </div>
+        <>
+            <Carousel />
+            <section>
+                {
+                    
+                }
+            </section>
+        </>
     )
 };
