8467937ea39a651f21607e4cbb704c5a9057293e
leonardog9823 <guanuco43@gmail.com>
2022-02-06 21:03:28 -0300

    beneficios

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Carousel } from '../components/Carousel';
 import { VentanaProducto } from '../components/VentanaProducto';
+import { Link } from 'react-router-dom';
 
 import "../styles/flex-item.scss";
 import "../styles/home.scss";
@@ -38,6 +39,36 @@ export const Home = () => {
                                 <VentanaProducto key={ producto.id } { ...producto }/>
                             ))
                         }
+                    </div>
+                </section>
+                <section className='subscripcion'>
+                    <div className='subscripcion__costo'>
+                        <p className='costo-nivel'>Subscribite al nivel 6</p>
+                        <div className='costo-info'>
+                            <p>Desde</p>
+                            <p><span className='info-precio'>$ 599</span>/mes</p>
+                            <p>Según tu nivel</p>
+                        </div>
+                    </div>
+                    <p className='subscripcion__sub'>Conseguí los mejores beneficios en Mercado Libre y Mercado Pago</p>
+                    <div className='costo-beneficios'>
+                        <div className='beneficios-info'>
+                            <img src="../../assets/d+.svg" alt="beneficio-1 disney" />
+                            <p>Disney+ sin cargo</p>
+                        </div>
+                        <div className='beneficios-info'>
+                            <img src="../../assets/star+.svg" alt="beneficio-2 star" />
+                            <p>Star+ sin cargo</p>
+                        </div>
+                        <div className='beneficios-info'>
+                            <img src="../../assets/truckgiftv4@2x.png" alt="beneficio-3 envios" />
+                            <p>Envíos gratis y rápidos desde $ 4.000 y 45% OFF en envíos de menos de $ 4.000</p>
+                        </div>
+                    </div>
+                    <div className='subscripcion__btn'>
+                        <div className='subscripcion-esp'>
+                            <Link to="/">Subscribete</Link>
+                        </div>
                     </div>
                 </section>
             </div>
